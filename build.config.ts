fa46145d707fbf18ac95e5ea4f12b94b44f1265a
Bewinxed <bewinxed@gmail.com>
2024-07-21 22:49:04 +0300

    chore: Remove unused build.config.ts file

--- build.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineBuildConfig } from 'unbuild';
-
-export default defineBuildConfig({
-  // Change outDir, default is 'dist'
-  outDir: 'dist',
-  failOnWarn: false,
-  // Generates .d.ts declaration file
-  declaration: true
-});
